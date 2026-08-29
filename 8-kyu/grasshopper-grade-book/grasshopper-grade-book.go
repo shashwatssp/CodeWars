b17514e95d59f042c6d414da39ebcaac045f5a35
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2026-08-29 20:43:20 +0530

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-grade-book/grasshopper-grade-book.go
@@ -0,0 +1,17 @@
+package kata
+​
+func GetGrade(a,b,c int) rune {
+    average:= (a+b+c)/3
+​
+    if average >= 90{
+    return 'A'
+    } else if average >= 80{
+      return 'B'
+    } else if average >= 70{
+      return 'C'
+    } else if average >= 60{
+      return 'D'
+    } else {
+      return 'F'
+    }
+}
